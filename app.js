eaa0cab0a94d88bb52aa3ae557c8c3f379028043
RodrigoSouza <rodrigo98ts@gmail.com>
2022-07-09 19:09:29 -0300

    voltando para o estado do paulo

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 // mais um console log de teste:
 console.log("O paulo fez esse commit sem vc ver na aula!")
-console.log("enviando para o github")
